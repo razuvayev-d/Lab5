cdcecd355a7e53137229b08c86e5db1c4e3e2cbe
razuvayev-d <46049860+razuvayev-d@users.noreply.github.com>
2020-04-10 12:30:35 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+namespace Lab5
+{
+    /// <summary>
+    /// Точка в декартовой системе координат
+    /// </summary>
+    public struct point
+    {
+        public double X { get; }
+        public double Y { get; }
+        public point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"({X}, {Y})";
+    }
+    /// <summary>
+    /// Точка полярных координат
+    /// </summary>
+    public struct polarpoint
+    {
+        /// <summary>
+        /// Радиальная координата
+        /// </summary>
+        public double Rad { get; }
+        /// <summary>
+        /// Полярный угол
+        /// </summary>
+        public double Angle { get; }
+        public polarpoint(double rad, double angle)
+        {
+            Rad = rad;
+            Angle = angle;
+        }
+    }
+
+   
+    /// <summary>
+    /// Целевой класс
+    /// </summary>
+     interface PolarCoords
+    {
+        
+        /// <summary>
+        /// Счиатет площадь фигуры, заданной массивом полярных координат
+        /// </summary>
+        /// <param name="PolCoords">Массив полярных координат</param>
+        /// <returns>Площадь фигуры</returns>
+        public double CalcArea(polarpoint[] PolCoords);
+       
+    }
+
+    
+    /// <summary>
+    /// Адаптер
+    /// </summary>
+    class ConvertCoord : PolarCoords
+    {
+        private DecartCoords DecCoords;
+       
+        public double CalcArea(polarpoint[] PolCoords)
+        {
+            DecCoords = new DecartCoords(PolarToDecartArr(PolCoords));         
+            return DecCoords.CalkArea();
+        }
+        /// <summary>
+        /// Преобразует полярные координаты точки в декартовы
+        /// </summary>
+        /// <param name="pol">Полярная точка</param>
+        /// <returns>Декартова точка</returns>
+        private point PolarToDecart(polarpoint pol)
+        {
+            double x, y;
+            x = pol.Rad * Math.Cos(DegToRad(pol.Angle));
+            y = pol.Rad * Math.Sin(DegToRad(pol.Angle));           
+            return new point(x,y);
+        }
+        /// <summary>
+        /// Преобразует массив полярных координат в массив декартовых координат
+        /// </summary>
+        /// <param name="PolArr">массив полярных координат</param>
+        /// <returns>массив декартовых координат</returns>
+        private point[] PolarToDecartArr(polarpoint[] PolArr)
+        {
+            point[] PointArr= new point[PolArr.Length];
+
+            for (int i = 0; i < PointArr.Length; i++)
+            {
+                PointArr[i] = PolarToDecart(PolArr[i]); 
+            }
+            return PointArr;
+        }
+        /// <summary>
+        /// Переводит градусы в радианы
+        /// </summary>
+        /// <param name="Deg">Градусы</param>
+        /// <returns>Радианы</returns>
+        private double DegToRad(double Deg)
+        {
+            return Deg * Math.PI / 180;
+        }
+    }
+
+    // "Adaptee"
+    /// <summary>
+    /// Адаптируемый
+    /// </summary>
+    class DecartCoords
+    {
+        /// <summary>
+        /// Расширенный массив координат. Реальные координаты индексируются 1..n-1
+        /// </summary>
+        point[] ExpCoords;
+
+        public DecartCoords(point[] Coords)
+        {
+            ExpCoords = ExpandArr(in Coords);
+        }
+        /// <summary>
+        /// Считает площадь фигуры по координатам
+        /// </summary>
+        /// <returns>Площадь фигуры</returns>
+        public double CalkArea()
+        {
+            double sum1 = 0;
+            double tmp;
+            for (int i = 1; i < ExpCoords.Length-1; i++)
+            {
+                tmp = ExpCoords[i].X * (ExpCoords[i + 1].Y - ExpCoords[i - 1].Y);
+                sum1 = sum1 + ExpCoords[i].X * (ExpCoords[i + 1].Y - ExpCoords[i - 1].Y);
+            }
+            return Math.Round(Math.Abs(sum1)/2,2);
+        }
+        /// <summary>
+        /// Добавляет в массив координат по одному элементу в начало и конец.
+        /// В начало добавляется последний элемент исходного массива.
+        /// В конец добавляется первый элемент исходного массива.        
+        /// </summary>
+        /// <param name="Coords">Входной массив</param>
+        /// <returns>Массив, в котором Coords лежит в индексах i=1..n-1</returns>
+        private point[] ExpandArr(in point[] Coords)
+        {
+            point[] ExpCoords = new point[Coords.Length + 2];
+            ExpCoords[0] = Coords[Coords.Length - 1];
+            ExpCoords[Coords.Length+1] = Coords[0];
+
+            for (int i = 0; i < Coords.Length; i++)
+            {
+                ExpCoords[i + 1] = Coords[i];
+            }
+            return ExpCoords;
+        }
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            // point[] arr = new point[3];
+
+            //// arr[0] = new point(0, 0);
+            //// arr[1] = new point(0, 2);
+            //// arr[2] = new point(4, 2);
+            ////arr[3] = new point(4, 0);
+            // arr[0] = new point(2, 1);
+            // arr[1] = new point(4, 5);
+            // arr[2] = new point(7, 8);
+
+            //polarpoint[] arr = new polarpoint[3];
+
+            //arr[0] = new polarpoint(4, 0);
+            //arr[1] = new polarpoint(4 * Math.Sqrt(2), 45);
+            //arr[2] = new polarpoint(0, 0);
+            // arr[3] = new polarpoint(0, 0);
+
+
+
+            //PolarCoords s = new ConvertCoord();
+
+            //Console.WriteLine(s.CalcArea(arr));
+            //Console.ReadKey();
+
+            Console.WriteLine("Введите число вершин фигуры");
+            int n;
+            try
+            {
+                if (!Int32.TryParse(Console.ReadLine(), out n)) throw new ArgumentException("Введено не число");
+                // Console.WriteLine("Введите полярные координаты точки парами через пробел");
+                polarpoint[] polarr = new polarpoint[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Введите полярные координаты точки через пробел");
+                    string str = Console.ReadLine();
+                    string[] ss = str.Split(" ");
+
+                    if (ss.Length != 2) throw new FormatException("Введено неверное количество координат");
+
+                    double rad, angle;
+                    if (!(Double.TryParse(ss[0], out rad) | Double.TryParse(ss[1], out angle)))
+                        throw new ArgumentException("Введено не число");
+
+                    polarr[i] = new polarpoint(rad, angle);
+                }
+
+                PolarCoords Adapt = new ConvertCoord();
+
+                Console.WriteLine("Площадь фигуры равна: ");
+                Console.WriteLine(Adapt.CalcArea(polarr));
+            }
+            catch (ArgumentException e)            
+            {
+                Console.WriteLine(e.Message);
+               
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Нажмите любую клавишу, чтобы завершить программу");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+
+
+        }
+    }
+}
